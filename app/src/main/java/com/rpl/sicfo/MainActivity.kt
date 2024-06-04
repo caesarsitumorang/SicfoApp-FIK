80a95c7d87f4211ee9a49beccf22b9a2a9325cf7
caesarsitumorang <caesarsitumorang16@gmail.com>
2024-06-04 16:43:41 +0700

    login,register,homepage donee

---
+++ app/src/main/java/com/rpl/sicfo/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.rpl.sicfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.rpl.sicfo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavBar by lazy {
+            binding.navView
+        }
+
+        val navView: BottomNavigationView = bottomNavBar
+        navView.itemActiveIndicatorColor = getColorStateList(R.color.white)
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navView.setupWithNavController(navController)
+
+    }
+}
