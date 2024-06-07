a0d66dde209f15eaeb3b13629ab2470215117624
caesarsitumorang <caesarsitumorang16@gmail.com>
2024-06-07 18:14:33 +0700

    add organisasi done

--- app/src/main/java/com/rpl/sicfo/ui/home/HomeFragment.kt
+++ app/src/main/java/com/rpl/sicfo/ui/home/HomeFragment.kt
@@ -2,11 +2,13 @@ package com.rpl.sicfo.ui.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -14,17 +16,22 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.rpl.sicfo.R
 import com.rpl.sicfo.adapter.BeritaOrganisasiAdapter
+import com.rpl.sicfo.adapter.OrganisasiFikomAdapter
 import com.rpl.sicfo.data.Berita
-import com.rpl.sicfo.data.User
+import com.rpl.sicfo.data.Organisasi
 import com.rpl.sicfo.databinding.FragmentHomeBinding
 import com.rpl.sicfo.ui.berita.DetailBeritaKegiatan
+import com.rpl.sicfo.ui.organisasiFikom.DetailOrganisasiFikomActivity
 
-class HomeFragment : Fragment(), BeritaOrganisasiAdapter.OnItemClickListener {
+class HomeFragment : Fragment(), BeritaOrganisasiAdapter.OnItemClickListener, OrganisasiFikomAdapter.OnItemClickListener {
 
     private lateinit var binding: FragmentHomeBinding
     private lateinit var database: DatabaseReference
-    private lateinit var adapter: BeritaOrganisasiAdapter
+    private lateinit var adapterberita: BeritaOrganisasiAdapter
+    private lateinit var adapterorganisasi : OrganisasiFikomAdapter
     private val beritaList = mutableListOf<Berita>()
+    private val organisasiList = mutableListOf<Organisasi>()
+    private lateinit var auth: FirebaseAuth
 
     override fun onItemClick(berita: Berita) {
         val intent = Intent(requireContext(), DetailBeritaKegiatan::class.java).apply {
@@ -36,9 +43,28 @@ class HomeFragment : Fragment(), BeritaOrganisasiAdapter.OnItemClickListener {
         startActivity(intent)
     }
 
+    override fun onItemClick(organisasi: Organisasi) {
+        val intent = Intent(requireContext(), DetailOrganisasiFikomActivity::class.java).apply {
+            putExtra("title", organisasi.title)
+            putExtra("logo", organisasi.logo)
+            putExtra("profil", organisasi.profil)
+            putExtra("image1", organisasi.image1)
+            putExtra("image2", organisasi.image2)
+            putExtra("image3", organisasi.image3)
+            putExtra("visiMisi", organisasi.visiMisi)
+            putExtra("fakultas", organisasi.fakultas)
+            putExtra("strukturalImage", organisasi.strukturalImage)
+            putExtra("anggota", organisasi.anggota)
+            putExtra("detailTitle", organisasi.detailTitle)
+        }
+        startActivity(intent)
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        database = FirebaseDatabase.getInstance().getReference("BeritaKegiatan")
+        database = FirebaseDatabase.getInstance().reference
+        auth = FirebaseAuth.getInstance()
     }
 
     override fun onCreateView(
@@ -46,21 +72,30 @@ class HomeFragment : Fragment(), BeritaOrganisasiAdapter.OnItemClickListener {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
-        setupRecyclerView()
+        setupRVBeritaKegiatan()
+        setupRVOrganisasiFikom()
         fetchDataFromFirebase()
-        fetchUserFullName()  // Fetch user full name
+        fetchOrganisasiDataFromFirebase()
+        fetchUserFullName()
         return binding.root
     }
 
-    private fun setupRecyclerView() {
-        adapter = BeritaOrganisasiAdapter(beritaList, this)
+    private fun setupRVOrganisasiFikom() {
+        adapterorganisasi = OrganisasiFikomAdapter(organisasiList, this)
+        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        binding.rvOrganisasi.layoutManager = layoutManager
+        binding.rvOrganisasi.adapter = adapterorganisasi
+    }
+
+    private fun setupRVBeritaKegiatan() {
+        adapterberita = BeritaOrganisasiAdapter(beritaList, this)
         val layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         binding.rvBeritaOrganisasi.layoutManager = layoutManager
-        binding.rvBeritaOrganisasi.adapter = adapter
+        binding.rvBeritaOrganisasi.adapter = adapterberita
     }
 
     private fun fetchDataFromFirebase() {
-        database.addValueEventListener(object : ValueEventListener {
+        database.child("BeritaKegiatan").addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 beritaList.clear()
                 for (dataSnapshot in snapshot.children) {
@@ -73,9 +108,8 @@ class HomeFragment : Fragment(), BeritaOrganisasiAdapter.OnItemClickListener {
                         beritaList.add(berita)
                     }
                 }
-                adapter.notifyDataSetChanged()
+                adapterberita.notifyDataSetChanged()
             }
-
 
             override fun onCancelled(error: DatabaseError) {
                 // Handle database error
@@ -83,20 +117,77 @@ class HomeFragment : Fragment(), BeritaOrganisasiAdapter.OnItemClickListener {
         })
     }
 
-    private fun fetchUserFullName() {
-        val userId = "user_id"  // Replace this with the actual user ID logic
-        val userRef = FirebaseDatabase.getInstance().getReference("Users").child(userId)
-        userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+    private fun fetchOrganisasiDataFromFirebase() {
+        database.child("OrganisasiFikom").addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                val user = snapshot.getValue(User::class.java)
-                if (user != null) {
-                    binding.tvUsername.text = user.fullName
+                organisasiList.clear()
+                for (dataSnapshot in snapshot.children) {
+                    val title = dataSnapshot.child("title").getValue(String::class.java)
+                    val logo = dataSnapshot.child("logo").getValue(String::class.java)
+                    val profil = dataSnapshot.child("profil").getValue(String::class.java)
+                    val fakultas = dataSnapshot.child("fakultas").getValue(String::class.java)
+                    val image1 = dataSnapshot.child("image1").getValue(String::class.java)
+                    val image2 = dataSnapshot.child("image2").getValue(String::class.java)
+                    val image3 = dataSnapshot.child("image3").getValue(String::class.java)
+                    val strukturalImage = dataSnapshot.child("strukturalImage").getValue(String::class.java)
+                    val visiMisi = dataSnapshot.child("visiMisi").getValue(String::class.java)
+                    val anggota = dataSnapshot.child("anggota").getValue(String::class.java)
+                    val detailTitle = dataSnapshot.child("detailTitle").getValue(String::class.java)
+
+                    if (title != null && logo != null && detailTitle != null && fakultas != null) {
+                        val organisasi = Organisasi(
+                            title = title,
+                            logo = logo,
+                            profil = profil ?: "",
+                            fakultas = fakultas,
+                            image1 = image1 ?: "",
+                            image2 = image2 ?: "",
+                            image3 = image3 ?: "",
+                            strukturalImage = strukturalImage ?: "",
+                            visiMisi = visiMisi ?: "",
+                            anggota = anggota ?: "",
+                            detailTitle = detailTitle
+                        )
+                        organisasiList.add(organisasi)
+                    }
                 }
+                Log.d("HomeFragment", "Total organisasi: ${organisasiList.size}")
+                adapterorganisasi.notifyDataSetChanged()
             }
 
             override fun onCancelled(error: DatabaseError) {
                 // Handle database error
             }
         })
+    }
+
+
+    private fun fetchUserFullName() {
+        val user = auth.currentUser
+        if (user != null) {
+            val userRef = database.child("Users").child(user.uid)
+            userRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        val namaLengkap = snapshot.child("namaLengkap").getValue(String::class.java)
+                        val kataPertama = namaLengkap!!.split(" ")[0]
+                        if (kataPertama != null) {
+                            binding.tvUsername.text = kataPertama
+                        } else {
+                            binding.tvUsername.text = "Nama tidak ditemukan"
+                        }
+                    } else {
+                        binding.tvUsername.text = "Pengguna tidak ditemukan"
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Handle database error
+                    binding.tvUsername.text = "Gagal mengambil data"
+                }
+            })
+        } else {
+            binding.tvUsername.text = "Pengguna tidak login"
+        }
     }
 }
